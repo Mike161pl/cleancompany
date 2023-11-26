d19b2ebbb1f7f309044e52029700b197b5332bec
mike161pl <mike161pl@gmail.com>
2023-11-26 15:21:28 +0100

    aded new photos and text

--- js/script.js
+++ js/script.js
@@ -2,12 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
 	const nav = document.querySelector('.navbar')
 	const allNavItems = document.querySelectorAll('.nav-link')
 	const navList = document.querySelector('.navbar-collapse')
+	const navButton = document.querySelector('.navbar-toggler')
+	const footerYear = document.querySelector('.footer__year')
 
 	function addShadow() {
-		if (window.scrollY >= 100) {
+		if (window.scrollY >= 50) {
 			nav.classList.add('shadow-bg')
 		} else {
 			nav.classList.remove('shadow-bg')
+		}
+	}
+	function buttonShadow() {
+		if (window.scrollY >= 0){
+			nav.classList.add('shadow-bg')
 		}
 	}
 
@@ -16,9 +23,12 @@ document.addEventListener('DOMContentLoaded', function () {
 			navList.classList.remove('show')
 		})
 	)
-
+	const handleCurrentYear = () => {
+		const year = new Date().getFullYear()
+		footerYear.innerText = year
+	}
 	
-
+	handleCurrentYear()
 	window.addEventListener('scroll', addShadow)
+	navButton.addEventListener('click', buttonShadow)
 })
-
